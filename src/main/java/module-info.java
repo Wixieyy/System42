e6e598b518068b78746b58bb1750d18e5c702880
Wixieyy <shendrickwilliams@gmail.com>
2024-04-25 16:39:38 +0200

    JavaFX base

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.system42 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.system42 to javafx.fxml;
+    exports org.example.system42;
+}
