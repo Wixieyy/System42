dbf99d0f379042a5fd1904f3e80ecff1e8b88234
Alex-xyc <zhang_xin_yuan@hotmail.com>
2024-05-03 09:54:31 +0200

    account aanmaken 1e commit

--- src/main/java/org/example/system42/HelloController.java
+++ src/main/java/org/example/system42/HelloController.java
@@ -1,7 +1,15 @@
 package org.example.system42;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -10,5 +18,17 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onMaakAccountButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("accountaanmaken.fxml"));
+        Parent newTemplate = fxmlLoader.load();
+
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(newTemplate, 800 , 600));
+        stage.show();
+
+
     }
 }
