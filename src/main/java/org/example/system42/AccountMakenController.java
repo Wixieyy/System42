2ee22c3f046a73976ba8829a755afbbe50d90aa6
Wix <shendrickwilliams@gmail.com>
2024-05-29 23:13:37 +0200

    Created login & gebruiker class, integrated login class into login page

--- src/main/java/org/example/system42/AccountMakenController.java
+++ src/main/java/org/example/system42/AccountMakenController.java
@@ -4,15 +4,24 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import org.bson.Document;
 
 public class AccountMakenController {
 
     protected void createAccount() {
-        String uri = "mongodb://localhost:27017";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("login-gegevens");
-            MongoCollection<Document> collection = database.getCollection("email");
+//        String uri = "mongodb://localhost:27017";
+//        try (MongoClient mongoClient = MongoClients.create(uri)) {
+//            MongoDatabase database = mongoClient.getDatabase("login-gegevens");
+//            MongoCollection<Document> collection = database.getCollection("email");
 //            if (!emailField.getText().equals(collection.find("email", emailField.getText()))) {
 //                if (passwordField1.getText().equals(passwordField2.getText())) {
 //                    collection.insertOne(new Document("gebruikersnaam", gebruikersnaamField.getText())
@@ -26,6 +35,26 @@ public class AccountMakenController {
 //            else {
 //                /*account already exists or email already used*/
 //            }
+//        }
+    }
+
+    @FXML
+    protected void onExitButtonClick() {
+        System.exit(0);
+    }
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+
+            Parent newTemplate = fxmlLoader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(newTemplate, 800, 600));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
