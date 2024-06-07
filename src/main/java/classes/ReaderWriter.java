8606d5bbfae658794d3b62624409f0afee4d467e
Wix <shendrickwilliams@gmail.com>
2024-06-07 10:39:23 +0200

    Refactored offline Json logic to ReaderWriter class

--- src/main/java/classes/ReaderWriter.java
+++ src/main/java/classes/ReaderWriter.java
@@ -5,10 +5,16 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ReaderWriter {
+    private static JSONObject responses;
     private static MongoClient mongoClient = null;
 
     /* Need to be on the same network (111.111.111.x) to be able to remotely read/write from database, I ain't setting up no port forwarding right now */
@@ -23,8 +29,25 @@ public class ReaderWriter {
         return collection;
     }
 
-    public void LocalReader() {
 
+
+    public static void OfflineJsonLoader() {
+        JSONParser parser = new JSONParser();
+        try (InputStreamReader reader = new InputStreamReader(ReaderWriter.class.getResourceAsStream("/org/example/system42/responses.json"))) {
+            responses = (JSONObject) parser.parse(reader);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String OfflineJsonReader(String userInput) {
+        for (Object key : responses.keySet()) {
+            String keyword = (String) key;
+            if (userInput.toLowerCase().contains(keyword.toLowerCase())) {
+                return (String) responses.get(keyword);
+            }
+        }
+        return "Sorry, I don't have that in my database!";
     }
 
     public Scanner LocalWriter() {
