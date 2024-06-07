15752d4b54790096aa953e67ed1a347c220aa82f
Wix <shendrickwilliams@gmail.com>
2024-06-07 09:21:43 +0200

    create classes & methods

---
+++ src/main/java/classes/Message.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Message {
+    private Sessie sessie;
+    private String vraag;
+    private String antwoord;
+
+    public String getMessage() {
+
+        return null;
+    }
+
+    public void setMessage(String message) {
+
+    }
+}
---
+++ src/main/java/classes/SessieManager.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class SessieManager {
+    public void executePrompt() {
+
+    }
+}
