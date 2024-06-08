fc51fa949e692ec60cf54eec06fa6fbd3efd8163
Wix <shendrickwilliams@gmail.com>
2024-06-08 12:16:03 +0200

    profiel bewerken styling

--- src/main/java/org/example/system42/ProfielBewerkenController.java
+++ src/main/java/org/example/system42/ProfielBewerkenController.java
@@ -6,14 +6,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ProfielBewerkenController {
-
+    @FXML
+    private TextField gebruikersnaamField;
+    @FXML
+    private TextField beroepField;
+    @FXML
+    private TextField afdelingField;
+    @FXML
+    private PasswordField wachtwoordField;
+    @FXML
+    private PasswordField herhaalWachtwoordField;
+    @FXML
+    private TextField wachtwoordTextField;
+    @FXML
+    private TextField herhaalWachtwoordTextField;
+    @FXML
+    private CheckBox toonWachtwoordCheckBox;
     @FXML
     protected void onOpslaanButton(ActionEvent event) throws IOException {
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("profiel-view.fxml"));
 
         Parent newTemplate = fxmlLoader.load();
@@ -22,6 +42,35 @@ public class ProfielBewerkenController {
         stage.setScene(new Scene(newTemplate, 800, 600));
         stage.show();
         stage.centerOnScreen();
+    }
+
+    @FXML
+    protected void onToonWachtwoordCheckBoxClicked() {
+        if (toonWachtwoordCheckBox.isSelected()) {
+            wachtwoordTextField.setText(wachtwoordField.getText());
+            wachtwoordField.setOpacity(0);
+            wachtwoordField.setDisable(true);
+            wachtwoordTextField.setDisable(false);
+            wachtwoordTextField.setOpacity(1);
+
+            herhaalWachtwoordTextField.setText(herhaalWachtwoordField.getText());
+            herhaalWachtwoordField.setOpacity(0);
+            herhaalWachtwoordField.setDisable(true);
+            herhaalWachtwoordTextField.setDisable(false);
+            herhaalWachtwoordTextField.setOpacity(1);
+        } else {
+            wachtwoordField.setText(wachtwoordTextField.getText());
+            wachtwoordTextField.setOpacity(0);
+            wachtwoordTextField.setDisable(true);
+            wachtwoordField.setDisable(false);
+            wachtwoordField.setOpacity(1);
+
+            herhaalWachtwoordField.setText(herhaalWachtwoordTextField.getText());
+            herhaalWachtwoordTextField.setOpacity(0);
+            herhaalWachtwoordTextField.setDisable(true);
+            herhaalWachtwoordField.setDisable(false);
+            herhaalWachtwoordField.setOpacity(1);
+        }
     }
 
     @FXML
