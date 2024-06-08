9c46e817cf88945a81322fc8dba96a67626a65c9
Wix <shendrickwilliams@gmail.com>
2024-06-08 21:23:10 +0200

    github actions stuff ignore

--- src/test/java/org/example/system42/TestTest.java
+++ src/test/java/org/example/system42/TestTest.java
@@ -1,16 +1,13 @@
-package org.example.system42;
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class TestTest {
-    //simple addition test
+
     @Test
-    public void testAddition() {
-        int a = 1;
-        int b = 2;
-        int sum = a + b;
-        assertEquals(3, sum);
+    public void sampleTest() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
     }
 }
